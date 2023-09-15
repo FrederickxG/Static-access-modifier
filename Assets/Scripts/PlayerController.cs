b6e8958351f0a223d8df9b3f5eed0ad7101251ad
FrederickxG <98033489+FrederickxG@users.noreply.github.com>
2023-09-15 14:05:11 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            GameManager.playerScore += 10; //Increase the players's score
+            Destroy(other.gameObject); //Destroy the collectible
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public Text scoreText;
+
+    private void Update()
+    {
+        // Update the displayed score with the value from the GameManager.
+        scoreText.text = "Score: " + GameManager.playerScore.ToString();
+    }
+}
